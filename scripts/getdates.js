4d60a1a87468c28d6c3030fdca3fd9bb892fc003
allwellejike <allwellejike16@gmail.com>
2026-01-14 00:34:45 +0100

    made corrections

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,8 @@ document.getElementById("currentyear").textContent = currentYear;
 
 // Display last modified date
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+const yearSpan = document.getElementById("currentyear");
+yearSpan.textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
